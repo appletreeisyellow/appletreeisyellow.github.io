c548038515812e6ec206841c526a91e72e4cb01a
Vivo Ye <yccvivo@gmail.com>
2020-08-21 12:13:53 -0700

    npm install more related pkgs for font awesome

---
+++ dev-log.md
@@ -0,0 +1,30 @@
+Install nuxt app
+
+```
+npx create-nuxt-app appletreeisyellow.github.io
+```
+
+Add `.gitignore` file. Initially, add the following lines:
+
+```
+.DS_Store
+node_modules/
+dist/
+.nuxt/
+```
+
+Running the dev mode. Then open `http://localhost:3000/` in browser.
+
+```
+npm run dev
+```
+
+Install Font Awesome
+
+```
+npm i --save nuxt-fontawesome
+npm i --save @fortawesome/free-brands-svg-icons
+npm i --save @fortawesome/free-solid-svg-icons
+```
+
+Here is a [guide](https://medium.com/@kozyreva.hanna/nuxt-js-fontawesome-integration-7ec56b1a41c8) helps you to walk through the nuxt-fontawesome installation
--- nuxt.config.js
+++ nuxt.config.js
@@ -3,28 +3,28 @@ export default {
    ** Nuxt rendering mode
    ** See https://nuxtjs.org/api/configuration-mode
    */
-  mode: 'universal',
+  mode: "universal",
   /*
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: 'static',
+  target: "static",
   /*
    ** Headers of the page
    ** See https://nuxtjs.org/api/configuration-head
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: process.env.npm_package_name || "",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || '',
+        hid: "description",
+        name: "description",
+        content: process.env.npm_package_description || "",
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
   /*
    ** Global CSS
@@ -45,16 +45,32 @@ export default {
    */
   buildModules: [
     // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module',
+    "@nuxtjs/eslint-module",
   ],
   /*
    ** Nuxt.js modules
    */
   modules: [
     // Doc: https://bootstrap-vue.js.org
-    'bootstrap-vue/nuxt',
+    "bootstrap-vue/nuxt",
     // Doc: https://github.com/nuxt/content
-    '@nuxt/content',
+    "@nuxt/content",
+    // Doc: https://www.npmjs.com/package/nuxt-fontawesome
+    [
+      "nuxt-fontawesome",
+      {
+        imports: [
+          {
+            set: "@fortawesome/free-solid-svg-icons",
+            icons: ["fas"],
+          },
+          {
+            set: "@fortawesome/free-brands-svg-icons",
+            icons: ["fab"],
+          },
+        ],
+      },
+    ],
   ],
   /*
    ** Content module configuration
@@ -66,4 +82,4 @@ export default {
    ** See https://nuxtjs.org/api/configuration-build/
    */
   build: {},
-}
+};
