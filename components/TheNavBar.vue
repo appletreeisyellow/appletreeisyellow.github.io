4b68f40660d60a3ce5712672b8fadcf834dd2a97
Vivo Ye <yccvivo@gmail.com>
2020-08-21 16:40:18 -0700

    add project in navbar

--- components/TheNavBar.vue
+++ components/TheNavBar.vue
@@ -1,8 +1,34 @@
 <template>
-  <nav class="navbar">
-    <nuxt-link :to="{ name: 'index' }" class="navbar-brand">
+  <nav class="navbar navbar-expand-lg navbar-light border-bottom">
+    <nuxt-link :to="{ name: 'index' }" class="navbar-brand text-dark">
       Chunchun Ye
     </nuxt-link>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <nuxt-link :to="{ name: 'index' }" class="nav-link">
+            Home <span class="sr-only">(current)</span>
+          </nuxt-link>
+        </li>
+        <li class="nav-item">
+          <nuxt-link :to="{ name: 'projects' }" class="nav-link">
+            Projects
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
   </nav>
 </template>
 
---
+++ pages/projects.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h1>My projects</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: "Chunchun Ye | Projects",
+      meta: [
+        { name: "twitter:title", content: "Chunchun Ye | Projects" },
+        // TODO: update description
+        {
+          name: "twitter:description",
+          content: "Check out all the software projects I worked on!",
+        },
+        //{ name: 'twitter:image', content: 'https://i.imgur.com/UYP2umJ.png'},
+        //{ name: 'twitter:card', content: 'summary_large_image'}
+        // TODO: facebook, instagram, ...
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
